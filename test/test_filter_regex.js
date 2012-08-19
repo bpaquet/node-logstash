58dc507c9613df08e42f5535ede61056b7f287f3
Bertrand Paquet <bpaquet@octo.com>
2012-08-19 15:33:25 +0200

    Add nginx access log parse test

--- test/test_filter_regex.js
+++ test/test_filter_regex.js
@@ -13,6 +13,11 @@ vows.describe('Filter regex ').addBatch({
     {'@message': 'abcd efgh ijk', '@fields': {fa: 'abcd', fb: 'efgh', fc: 'toto'}},
     {'@message': 'abcdefghijk'},
   ]),
+  'with star': filter_helper.create('regex', '?regex=^(\\S*) (\\S+)&fields=fa,fb', [
+    {'@message': ' efgh ijk'},
+  ], [
+    {'@message': ' efgh ijk', '@fields': {fb: 'efgh'}},
+  ]),
   'type filtering': filter_helper.create('regex', '?type=toto&regex=^(\\S+) (\\S+)&fields=fa,fb', [
     {'@message': 'abcd efgh ijk'},
     {'@message': 'abcd efgh ijk', '@type': 'toto'},
@@ -31,5 +36,22 @@ vows.describe('Filter regex ').addBatch({
     {'@message': 'abcd efgh ijk'},
   ], [
     {'@message': 'abcd efgh ijk', '@fields': {fa: 'abcd'}},
+  ]),
+  'nginx parsing': filter_helper.create('regex', '?regex=^(\\S+) - (\\S*) ?- \\[([^\\]]+)\\] "(\\S+) (\\S+) \\S+" (\\d+) (\\d+) "([^"]*)" "([^"]*)"&fields=ip,user,timestamp,method,url,status,bytes_sent,referer,user_agent', [
+    {'@message': '127.0.0.1 - - [31/Jul/2012:18:02:28 +0200] "GET /favicon.ico HTTP/1.1" 502 574 "-" "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_4) AppleWebKit/537.2 (KHTML, like Gecko) Chrome/22.0.1215.0 Safari/537.2"'},
+  ],[
+    {
+      '@message': '127.0.0.1 - - [31/Jul/2012:18:02:28 +0200] "GET /favicon.ico HTTP/1.1" 502 574 "-" "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_4) AppleWebKit/537.2 (KHTML, like Gecko) Chrome/22.0.1215.0 Safari/537.2"',
+      '@fields': {
+        ip: '127.0.0.1',
+        timestamp: '31/Jul/2012:18:02:28 +0200',
+        method: 'GET',
+        status: 502,
+        bytes_sent: 574,
+        user_agent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_4) AppleWebKit/537.2 (KHTML, like Gecko) Chrome/22.0.1215.0 Safari/537.2',
+        referer: '-',
+        url: '/favicon.ico'
+      }
+    }
   ]),
 }).export(module);
