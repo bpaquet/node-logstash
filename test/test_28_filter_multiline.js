3b228a8fdf2d947e371443d15126c0eb8f7e4197
Bertrand Paquet <bpaquet@octo.com>
2012-10-28 23:54:51 +0100

    Add multiline & split filter
    Fix #8 #9

---
+++ test/test_28_filter_multiline.js
@@ -0,0 +1,33 @@
+var vows = require('vows'),
+    assert = require('assert'),
+    os = require('os'),
+    filter_helper = require('./filter_helper');
+
+vows.describe('Filter multiline ').addBatch({
+  'normal': filter_helper.create('multiline', '?start_line_regex=^abc', [
+    {'@message': 'abc', '@source_host': 'a'},
+    {'@message': 'def', '@source_host': 'a'},
+    {'@message': 'abc', '@source_host': 'a'},
+    {'@message': '123', '@source_host': 'a'},
+  ], [
+    {'@message': 'abc\ndef', '@source_host': 'a'},
+    {'@message': 'abc\n123', '@source_host': 'a'},
+  ]),
+  'wrong initial content': filter_helper.create('multiline', '?start_line_regex=^abc', [
+    {'@message': 'def', '@source_host': 'a'},
+    {'@message': 'abc', '@source_host': 'a'},
+    {'@message': '123', '@source_host': 'a'},
+  ], [
+    {'@message': 'def', '@source_host': 'a'},
+    {'@message': 'abc\n123', '@source_host': 'a'},
+  ]),
+  'multiple sources': filter_helper.create('multiline', '?start_line_regex=^abc', [
+    {'@message': 'abc', '@source_host': 'a'},
+    {'@message': 'abc', '@source_host': 'b'},
+    {'@message': 'def', '@source_host': 'a'},
+    {'@message': '123', '@source_host': 'b'},
+  ], [
+    {'@message': 'abc\ndef', '@source_host': 'a'},
+    {'@message': 'abc\n123', '@source_host': 'b'},
+  ]),
+}).export(module);
