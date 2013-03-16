095f6ad4eb85c4379d855cc7fabf66621daae3bd
Bertrand Paquet <bpaquet@octo.com>
2013-03-16 19:53:08 +0100

    Allow to configure node_version for tests, and fix deps for node 0.10.0

--- remote_test.sh
+++ remote_test.sh
@@ -7,11 +7,16 @@ if [ "$TARGET" = "" ]; then
   exit 1
 fi
 
+if [ "$NODE_VERSION" = "" ]; then
+  NODE_VERSION=0.8.16
+fi
+
 COMMAND="npm test"
 
 if [ "$TEST" != "" ]; then
   COMMAND="TEST='$TEST' npm test"
 fi
 
-rsync -avh --delete --exclude=.git --exclude=node_modules ../node-logstash/ $TARGET:node-logstash/
-ssh $TARGET "source .nvm/nvm.sh && nvm use v`cat .node_version` && cd node-logstash && $COMMAND"
+echo "Using node version $NODE_VERSION"
+rsync -avh --delete --exclude=.git --exclude=node_modules ../node-logstash/ $TARGET:node-logstash_$NODE_VERSION/
+ssh $TARGET "source .nvm/nvm.sh && nvm use v$NODE_VERSION && cd node-logstash_$NODE_VERSION && $COMMAND"
