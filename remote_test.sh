c1abdda32f6fceff84703cad771dc4742e83f519
Bertrand Paquet <bertrand.paquet@gmail.com>
2013-12-07 11:25:43 +0100

    ZeroMQ 3.2.4 compatibility

--- remote_test.sh
+++ remote_test.sh
@@ -21,6 +21,10 @@ if [ "$COVER" != "" ]; then
   COMMAND="COVER=$COVER $COMMAND"
 fi
 
+if [ "SSH_LD_LIBRARY_PATH" != "" ]; then
+	COMMAND="LD_LIBRARY_PATH=$SSH_LD_LIBRARY_PATH $COMMAND"
+fi
+
 echo "Using node version $NODE_VERSION"
 rsync -avh --delete --exclude=.git --exclude=node_modules --exclude=coverage ../node-logstash/ $TARGET:node-logstash_$NODE_VERSION/
 ssh $TARGET "source .nvm/nvm.sh && nvm use v$NODE_VERSION && cd node-logstash_$NODE_VERSION && echo $NODE_VERSION > .node_version && $COMMAND"
