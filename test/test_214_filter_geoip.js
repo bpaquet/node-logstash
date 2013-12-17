bd8fa61350c3516aaebc9b5c465e220fc6fbea5f
Bertrand Paquet <bertrand.paquet@gmail.com>
2013-12-17 23:04:37 +0100

    Make geoip test more resilient

--- test/test_214_filter_geoip.js
+++ test/test_214_filter_geoip.js
@@ -1,36 +1,43 @@
 var vows = require('vows'),
+    geoip = require('geoip-lite'),
     filter_helper = require('./filter_helper');
+
+var ip1 = '91.121.153.187';
+var ip1_res = geoip.lookup(ip1);
+
+var ip2 = '82.66.65.173';
+var ip2_res = geoip.lookup(ip2);
 
 vows.describe('Filter Geoip ').addBatch({
   'normal': filter_helper.create('geoip', 'ip', [
     {'titi': 'tata'},
     {'titi': 'tata', 'ip': 'toto'},
-    {'titi': 'tata', 'ip': '91.121.153.187'},
-    {'titi': 'tata', 'ip': '82.66.65.173'},
+    {'titi': 'tata', 'ip': ip1},
+    {'titi': 'tata', 'ip': ip2},
   ], [
     {'titi': 'tata'},
     {'titi': 'tata', 'ip': 'toto'},
-    {'titi': 'tata', 'ip': '91.121.153.187', 'ip_geo_country': 'FR', 'ip_geo_lonlat': [ 2.35, 48.86 ] },
-    {'titi': 'tata', 'ip': '82.66.65.173', 'ip_geo_country': 'FR', 'ip_geo_region': 'A8', 'ip_geo_city': 'Vincennes', 'ip_geo_lonlat': [ 2.4333, 48.85 ]},
+    {'titi': 'tata', 'ip': ip1, 'ip_geo_country': ip1_res.country, 'ip_geo_lonlat': [ip1_res.ll[1], ip1_res.ll[0]] },
+    {'titi': 'tata', 'ip': ip2, 'ip_geo_country': ip2_res.country, 'ip_geo_region': ip2_res.region, 'ip_geo_city': ip2_res.city, 'ip_geo_lonlat': [ip2_res.ll[1], ip2_res.ll[0]]},
   ]),
   'hide city': filter_helper.create('geoip', 'ip?city_field=none', [
-    {'titi': 'tata', 'ip': '82.66.65.173'},
+    {'titi': 'tata', 'ip': ip2},
   ], [
-    {'titi': 'tata', 'ip': '82.66.65.173', 'ip_geo_country': 'FR', 'ip_geo_region': 'A8', 'ip_geo_lonlat': [ 2.4333, 48.85 ]},
+    {'titi': 'tata', 'ip': ip2, 'ip_geo_country': ip2_res.country, 'ip_geo_region': ip2_res.region, 'ip_geo_lonlat': [ip2_res.ll[1], ip2_res.ll[0]]},
   ]),
   'hide country': filter_helper.create('geoip', 'ip?country_field=none', [
-    {'titi': 'tata', 'ip': '82.66.65.173'},
+    {'titi': 'tata', 'ip': ip2},
   ], [
-    {'titi': 'tata', 'ip': '82.66.65.173', 'ip_geo_region': 'A8', 'ip_geo_city': 'Vincennes', 'ip_geo_lonlat': [ 2.4333, 48.85 ]},
+    {'titi': 'tata', 'ip': ip2, 'ip_geo_region': ip2_res.region, 'ip_geo_city': ip2_res.city, 'ip_geo_lonlat': [ip2_res.ll[1], ip2_res.ll[0]]},
   ]),
   'hide region': filter_helper.create('geoip', 'ip?region_field=none', [
-    {'titi': 'tata', 'ip': '82.66.65.173'},
+    {'titi': 'tata', 'ip': ip2},
   ], [
-    {'titi': 'tata', 'ip': '82.66.65.173', 'ip_geo_country': 'FR', 'ip_geo_city': 'Vincennes', 'ip_geo_lonlat': [ 2.4333, 48.85 ]},
+    {'titi': 'tata', 'ip': ip2, 'ip_geo_country': ip2_res.country, 'ip_geo_city': ip2_res.city, 'ip_geo_lonlat': [ip2_res.ll[1], ip2_res.ll[0]]},
   ]),
   'hide lonlat': filter_helper.create('geoip', 'ip?lonlat_field=none', [
-    {'titi': 'tata', 'ip': '82.66.65.173'},
+    {'titi': 'tata', 'ip': ip2},
   ], [
-    {'titi': 'tata', 'ip': '82.66.65.173', 'ip_geo_country': 'FR', 'ip_geo_region': 'A8', 'ip_geo_city': 'Vincennes'},
+    {'titi': 'tata', 'ip': ip2, 'ip_geo_country': ip2_res.country, 'ip_geo_region': ip2_res.region, 'ip_geo_city': ip2_res.city},
   ]),
 }).export(module);
