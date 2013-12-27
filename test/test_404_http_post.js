4fb554092500cea932cf7acede3a40d606751a37
Bertrand Paquet <bertrand.paquet@gmail.com>
2013-12-27 19:04:05 +0100

    Fix content type for output http post, #46

--- test/test_404_http_post.js
+++ test/test_404_http_post.js
@@ -4,14 +4,14 @@ var vows = require('vows-batch-retry'),
     assert = require('assert'),
     helper = require('./integration_helper.js');
 
-vows.describe('Integration Http post :').addBatchRetry({
-  'http_post test': {
+function createHttpTest(output_url, check_callback) {
+  return {
     topic: function() {
       var callback = this.callback;
       var reqs = [];
       helper.createAgent([
         'input://tcp://0.0.0.0:17874?type=pouet',
-        'output://http_post://localhost:17875?path=/#{type}',
+        output_url,
       ], function(agent) {
         var http_server = http.createServer(function(req, res) {
           var body = '';
@@ -42,9 +42,23 @@ vows.describe('Integration Http post :').addBatchRetry({
       assert.ifError(err);
       assert.equal(reqs.length, 1);
 
-      assert.equal(reqs[0].req.method, 'POST');
-      assert.equal(reqs[0].req.url, '/pouet');
-      assert.equal(reqs[0].body, 'toto');
+      check_callback(reqs);
     }
-  },
+  };
+}
+
+vows.describe('Integration Http post :').addBatchRetry({
+  'http_post test raw': createHttpTest('output://http_post://localhost:17875?path=/#{type}', function(reqs) {
+    assert.equal(reqs[0].req.method, 'POST');
+    assert.equal(reqs[0].req.headers['content-type'], 'text/plain');
+    assert.equal(reqs[0].req.url, '/pouet');
+    assert.equal(reqs[0].body, 'toto');
+  }),
+}, 5, 20000).addBatchRetry({
+  'http_post test json': createHttpTest('output://http_post://localhost:17875?path=/#{type}&serializer=json_logstash', function(reqs) {
+    assert.equal(reqs[0].req.method, 'POST');
+    assert.equal(reqs[0].req.headers['content-type'], 'application/json');
+    assert.equal(reqs[0].req.url, '/pouet');
+    helper.checkResult(reqs[0].body, {message: 'toto', host: '127.0.0.1', tcp_port: 17874, type: 'pouet', '@version': '1'});
+  }),
 }, 5, 20000).export(module);
